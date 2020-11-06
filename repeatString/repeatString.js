f314ee609160cbb8379bd9aac1a56780f6976e21
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2020-11-06 18:06:49 -0500

    TOP first commit

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,24 @@
-const repeatString = function() {
+// const repeatString = function() {
 
+// }
+
+// module.exports = repeatString
+
+function capitalize(string) {
+    str = string.toLowerCase();
+    first = str[0].toUpperCase();
+    capitalized = first + str.slice(1);
+    console.log(capitalized);
 }
 
-module.exports = repeatString
+capitalize('bOrAT'); //returns: Borat
+
+function lastLetter(streeng) {
+    last = streeng.slice(-1);
+    console.log(last);
+}
+
+lastLetter("Westwood"); //returns: 'd'
+lastLetter("abcd"); //returns: 'd'
+
+
